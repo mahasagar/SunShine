c4f72b0dbdda5f8179b893ef75abe0e81dd3692b
mahasagar <sagar.chaudhari@medibox.in>
2016-05-19 23:53:33 +0530

    Part One

---
+++ app/src/main/java/com/mahasagar/sunshine/DetailActivity.java
@@ -0,0 +1,113 @@
+package com.mahasagar.sunshine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.v7.widget.ShareActionProvider;
+
+
+/**
+ * Created by mahasagar on 17/5/16.
+ */
+public class DetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+        private static final String LOG_TAG = PlaceholderFragment.class.getSimpleName();
+        private String mForecastStr;
+
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            Intent intent = getActivity().getIntent();
+            String message = intent.getStringExtra(Intent.EXTRA_TEXT);
+            TextView details_text = (TextView)rootView.findViewById(R.id.details_text);
+            details_text.setText(message);
+            Toast.makeText(getActivity(),"message : "+message,Toast.LENGTH_SHORT).show();
+
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.details_text))
+                        .setText(mForecastStr);
+            }
+            return rootView;
+        }
+
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.details_fragment, menu);
+
+            // Retrieve the share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            // Get the provider and hold onto it to set/change the share intent.
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+            // Attach an intent to this ShareActionProvider.  You can update this at any time,
+            // like when the user selects a new piece of data they might like to share.
+            if (mShareActionProvider != null ) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "Share Action Provider is null?");
+            }
+        }
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+
+
+            return super.onOptionsItemSelected(item);
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                    mForecastStr + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/mahasagar/sunshine/common/Constants.java
@@ -0,0 +1,10 @@
+package com.mahasagar.sunshine.common;
+
+/**
+ * Created by mahasagar on 4/4/16.
+ */
+public class Constants {
+    public static String APP_KEY="@@@@@@@@@@@ API KEY @@@@@@@@@@@@@@";
+    public static String W_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=";
+    public static String W_URL_MODE = "&mode=json&units=metric&cnt=7&APPID=";
+}
